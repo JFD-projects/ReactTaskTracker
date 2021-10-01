db3278457f19d6555eda53d24e8ca8f751352955
Руслан Сколотий <ruslanskolotij@MacBook-Pro-Ruslan.local>
2021-10-01 14:57:28 +0300

    register and separate tasks

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,4 @@
+import Auth from "../services/auth"
 let columns = [
   { id: 1, title: "Новые", color: "#03a9f4" },
   { id: 2, title: "Анализ", color: "yellow" },
@@ -15,6 +16,7 @@ let tasks = [
     active: 1,
     responsible: "Иванов",
     deadLine: "2022-01-01",
+    userId: 1,
   },
   {
     id: 2,
@@ -24,6 +26,7 @@ let tasks = [
     active: 1,
     responsible: "Иванов",
     deadLine: "2022-01-01",
+    userId: 1,
   },
   {
     id: 3,
@@ -33,24 +36,48 @@ let tasks = [
     active: 1,
     responsible: "Иванов",
     deadLine: "2022-01-01",
+    userId: 1,
+  },
+  {
+    id: 4,
+    text: "Пример готовой задчи",
+    title: "Готовая задачка",
+    columnId: 5,
+    active: 1,
+    responsible: "Иванов",
+    deadLine: "2022-01-01",
+    userId: 2,
   },
 ]
 
-let users = [{ id: 1, login: "demo", password: "demo", token: "73y4287rt687r36", name: "DEMO" }]
+let users = [{ id: 1, login: "demo", password: "demo", token: "73y4287rt687r36", name: "DEMO" },
+{ id: 2, login: "demo2", password: "demo2", token: "furifjwiurjfi3223", name: "DEMO2" }]
+
+const getUserIdByToken = (token) => {
+  const user = users.find((user) => user.token === token)
+  if (user) return user.id
+  return false
+}
 
 const Api = {
-  getTasks: (userId) => {
+  getTasks: () => {
+    const token = new Auth().getToken()
+    const userId = getUserIdByToken(token)
+
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        resolve(tasks)
+        resolve(tasks.filter((item) => item.userId === userId))
       }, 100)
     })
   },
   getTaskById: (taskId) => {
+    const token = new Auth().getToken()
+    const userId = getUserIdByToken(token)
+
     taskId = Number(taskId)
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        resolve(tasks.find((task) => task.id === taskId))
+        resolve(tasks.find((task) => task.id === taskId && task.userId === userId))
       }, 100)
     })
   },
@@ -62,19 +89,25 @@ const Api = {
     })
   },
   addTask: (data) => {
+    const token = new Auth().getToken()
+    const userId = getUserIdByToken(token)
+
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         const index = Math.max(...tasks.map((item) => item.id)) + 1
-        tasks = [...tasks, { text: "", title: "", columnId: 1, active: 1, ...data, id: index }]
+        tasks = [...tasks, { text: "", title: "", columnId: 1, active: 1, ...data, id: index, userId: userId }]
         resolve(true)
       }, 100)
     })
   },
   updateTask: (id, data) => {
+    const token = new Auth().getToken()
+    const userId = getUserIdByToken(token)
+
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         id = Number(id)
-        const findIndex = tasks.findIndex((item) => item.id === id)
+        const findIndex = tasks.findIndex((item) => item.id === id && item.userId === userId)
         if (findIndex !== -1) {
           tasks[findIndex] = { ...tasks[findIndex], ...data }
         }
@@ -90,6 +123,21 @@ const Api = {
           resolve(user)
         } else {
           reject("Пользователь не найден")
+        }
+      }, 1000)
+    })
+  },
+  register: ({ login, password, name }) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const user = users.find((user) => user.login === login)
+        if (user) {
+          reject("Пользователь с таким логином уже существует")
+        } else {
+          const newId = Math.max(...users.map((item) => item.id)) + 1
+          const user = { id: newId, login, password, token: Date.now(), name }
+          users.push(user)
+          resolve(user)
         }
       }, 1000)
     })
--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,34 +1,76 @@
-import React from "react"
+import React, { useState } from "react"
 import "./register.css"
+import { Formik, Field, Form } from "formik"
+import Api from "../../api/api"
+import Auth from "../../services/auth"
+import { useHistory } from "react-router-dom"
+import Loading from "../../components/loading/loading"
 
 const Register = () => {
+  const history = useHistory()
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState("")
+
+  const onSubminHandler = (values) => {
+    setError()
+    setLoading(true)
+    Api.register(values)
+      .then((user) => {
+        const auth = new Auth()
+        auth.signin(user.name, user.token)
+        setTimeout(() => {
+          history.replace("/")
+        })
+      })
+      .catch((error) => {
+        setError(error)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }
+
   return (
     <div className="text-center form-signup-containter">
+      <Loading hidden={!loading} />
       <main className="form-signup">
-        <form>
-          <h1 className="h3 mb-3 fw-normal">Регистрация</h1>
+        <Formik initialValues={{ name: "", login: "", password: "" }} onSubmit={onSubminHandler}>
+          <Form>
+            <h1 className="h3 mb-3 fw-normal">Регистрация</h1>
 
-          <div className="form-floating">
-            <input type="text" className="form-control" id="floatingInput" placeholder="Имя" />
-            <label htmlFor="floatingInput">Имя</label>
-          </div>
-          <div className="form-floating">
-            <input type="email" className="form-control" id="floatingInput" placeholder="Логин" />
-            <label htmlFor="floatingInput">Логин</label>
-          </div>
-          <div className="form-floating">
-            <input type="password" className="form-control" id="floatingPassword" placeholder="Пароль" />
-            <label htmlFor="floatingPassword">Пароль</label>
-          </div>
-          <div className="mb-3">
-            <button className="w-100 btn btn-lg btn-primary" type="submit">
-              Зарегистрироваться
-            </button>
-          </div>
-          <div className="mb-3">
-            <a href="/login"> Авторизация</a>
-          </div>
-        </form>
+            {error && (
+              <div className="alert alert-danger" role="alert">
+                {error}
+              </div>
+            )}
+            <div className="form-floating">
+              <Field name="name" type="text" className="form-control" id="floatingInput" placeholder="Имя" />
+              <label htmlFor="floatingInput">Имя</label>
+            </div>
+            <div className="form-floating">
+              <Field name="login" type="text" className="form-control" id="floatingInput" placeholder="Логин" />
+              <label htmlFor="floatingInput">Логин</label>
+            </div>
+            <div className="form-floating">
+              <Field
+                name="password"
+                type="password"
+                className="form-control"
+                id="floatingPassword"
+                placeholder="Пароль"
+              />
+              <label htmlFor="floatingPassword">Пароль</label>
+            </div>
+            <div className="mb-3">
+              <button className="w-100 btn btn-lg btn-primary" type="submit">
+                Зарегистрироваться
+              </button>
+            </div>
+            <div className="mb-3">
+              <a href="/login"> Авторизация</a>
+            </div>
+          </Form>
+        </Formik>
       </main>
     </div>
   )
